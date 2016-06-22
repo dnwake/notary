0e3b3daa3756878e5b7454a46b9112d92d7d02a9
David Wake <dwake@box.com>
2016-06-22 16:31:55 -0700

    Fix syntax error

--- cryptoservice/certificate.go
+++ cryptoservice/certificate.go
@@ -18,7 +18,7 @@ func GenerateCertificate(rootKey data.PrivateKey, gun string, startTime, endTime
 
 // GenerateCertificate generates an X509 Certificate from a template, given a GUN and validity interval.
 // The certificate is signed by ca if non-nil; otherwise it is self-signed.
-func GenerateSignedCertificate(rootKey data.PrivateKey, gun string, startTime, endTime time.Time, *x509.Certificate ca) (*x509.Certificate, error) {
+func GenerateSignedCertificate(rootKey data.PrivateKey, gun string, startTime, endTime time.Time, ca *x509.Certificate) (*x509.Certificate, error) {
 	signer := rootKey.CryptoSigner()
 	if signer == nil {
 		return nil, fmt.Errorf("key type not supported for Certificate generation: %s\n", rootKey.Algorithm())
@@ -28,7 +28,7 @@ func GenerateSignedCertificate(rootKey data.PrivateKey, gun string, startTime, e
 }
 
 
-func generateCertificate(signer crypto.Signer, gun string, startTime, endTime time.Time, *x509.Certificate ca) (*x509.Certificate, error) {
+func generateCertificate(signer crypto.Signer, gun string, startTime, endTime time.Time, ca *x509.Certificate) (*x509.Certificate, error) {
 	template, err := trustmanager.NewCertificate(gun, startTime, endTime)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create the certificate template for: %s (%v)", gun, err)
